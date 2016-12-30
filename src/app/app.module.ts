464c8f0dec610e0c64860c7ed123d68b6b2f6afd
Andrew Wyborski <wyborskid@gmail.com>
2016-12-30 00:10:36 -0500

    Added an init link for heroes list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule }        from '@angular/platform-browser';
 import { NgModule }             from '@angular/core';
 import { FormsModule }          from '@angular/forms';
 import { HttpModule }           from '@angular/http';
+import { RouterModule }         from '@angular/router';
 import { NgbModule }            from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent }         from './app.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroService }          from './providers/hero.service';
+
+
 
 @NgModule({
   declarations: [
@@ -19,7 +22,12 @@ import { HeroService }          from './providers/hero.service';
     BrowserModule,
     FormsModule,
     HttpModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot([{
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
   providers: [
     HeroService
