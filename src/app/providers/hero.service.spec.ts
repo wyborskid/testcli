ce4048ac3a4821cd5b68a66c46f7fca71f9904c5
Andrew Wyborski <wyborskid@gmail.com>
2016-12-29 22:50:29 -0500

    getting latest onto GitHub

--- src/app/providers/hero.service.spec.ts
+++ src/app/providers/hero.service.spec.ts
@@ -1,16 +1,16 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
-import { HeroServiceService } from './hero-service.service';
+import { HeroService } from './hero.service';
 
 describe('HeroServiceService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HeroServiceService]
+      providers: [HeroService]
     });
   });
 
-  it('should ...', inject([HeroServiceService], (service: HeroServiceService) => {
+  it('should ...', inject([HeroService], (service: HeroService) => {
     expect(service).toBeTruthy();
   }));
 });
